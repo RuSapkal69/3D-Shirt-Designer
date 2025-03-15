d76aa97eac2c94d0f54769ef5be775976367c9fe
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-03-16 03:24:29 +0530

    Work in progress

--- frontend/src/pages/Customizer.jsx
+++ frontend/src/pages/Customizer.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSnapshot } from 'valtio'
+import { AnimatePresence, motion } from 'framer-motion'
+
+import config from '../config'
+import state from '../store'
+import { download } from '../assets'
+import { downloadCanvasImage, reader } from '../config/helpers';
+import { Editor, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+
 
 const Customizer = () => {
   return (
