aca1711b7da82c79edd0b3a6a2ff2534f12f6766
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-03-16 03:07:32 +0530

    Writing code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className='text-slate-600 font-extrabold'>
-      hello
-    </div>
+    <main className='app transition-all ease-in'>
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   )
 }
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation,
+} from '../config/motion';
+import state from '../store';
+
+
+const Home = () => {
+
+    const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+        {snap.intro && (
+            <motion.section className='home' {...slideAnimation("left")}>
+                <motion.header {...slideAnimation("down")}>
+                  <img 
+                  src='./threejs.png'
+                  alt="logo"
+                  className='w-8 h-8 object-contain'
+                  />  
+                </motion.header>
+                <motion.div className='home-content' {...headContainerAnimation}>
+                    <motion.div {...headTextAnimation}>
+                        <h1 className='head-text'>
+                            LET'S <br className='xl:block hidden' /> DO IT.
+                        </h1>
+                    </motion.div>
+                    <motion.div>
+                        <p>
+                            Create your own unique and exclusive shirt with our brand-new
+                            3D customization tool. <strong>Unleash your imagination</strong>{" "}
+                            and define your own style.
+                        </p>
+                    </motion.div>
+                </motion.div>
+            </motion.section>
+        )}
+    </AnimatePresence>
+  )
+}
+
+export default Home
