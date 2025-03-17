e4ed32c2cc1313b413aed25702e7dbd4e4c8b62a
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-03-18 00:00:31 +0530

    Error fixed

--- frontend/src/canvas/Shirt.jsx
+++ frontend/src/canvas/Shirt.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber'
+import { useSnapshot } from 'valtio'
+import {Decal, useGLTF, useTexture} from '@react-three/drei'
+import state from '../store'
 
 const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+
+  const logoTexture = useTexture(snap.logoDecal);
+  
+
   return (
     <div>
       
