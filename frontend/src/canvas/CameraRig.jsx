9fb1bcfd422a374809c07ce4ed833158707cbafa
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-03-19 23:39:43 +0530

    Error fixing

--- frontend/src/canvas/CameraRig.jsx
+++ frontend/src/canvas/CameraRig.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+import { easing } from 'maath'
 
-const CameraRig = () => {
+const CameraRig = ({children}) => {
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  easing.dampE(
+    group.current.rotation,
+    [state.pointer.y / 10, -state.pointer.x / 5, 0],
+    0.25,
+  )
   return (
-    <div>
-      
-    </div>
+    <group>
+      {children}
+    </group>
   )
 }
 
