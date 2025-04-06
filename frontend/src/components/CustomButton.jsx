d31cacc668d28ada9b80d8a4c126c7c9ef31eb5b
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-04-06 19:54:19 +0530

    Errors mostly fixed, background color changed!

--- frontend/src/components/CustomButton.jsx
+++ frontend/src/components/CustomButton.jsx
@@ -1,29 +1,36 @@
 import React from 'react'
-import state from '../store'
-import { useSnapshot } from 'valtio'
+import { useSnapshot } from 'valtio';
 
-const CustomButton = ({type, title, customStyles, handleClick}) => {
+import state from '../store';
+import { getContrastingColor } from '../config/helpers';
 
-    const snap = useSnapshot(state);
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
 
-    const generateStyles = (type) => {
-        if(type === 'filled') {
-            return {
-                backgroundColor: snap.color,
-                color: '#fff',
-            }
-        }
+  const generateStyle = (type) => {
+    if(type === 'filled') {
+      return {
+        backgroundColor: snap.color,
+        color: getContrastingColor(snap.color)
+      }
+    } else if(type === "outline") {
+      return {
+        borderWidth: '1px',
+        borderColor: snap.color,
+        color: snap.color
+      }
     }
+  }
 
   return (
-    <button className={'px-2 py-1.5 flex-1 rounded-md ${customStyles}'}
-    style={generateStyles(type)}
-    onClick={handleClick}
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
     >
-        {title}
+      {title}
     </button>
   )
 }
-
 
 export default CustomButton
