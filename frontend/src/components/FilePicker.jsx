46ad42b8efe73c488717fbf39dc392e2e4395e60
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-04-10 23:56:26 +0530

    Another UI added for testing

--- frontend/src/components/FilePicker.jsx
+++ frontend/src/components/FilePicker.jsx
@@ -3,24 +3,21 @@ import React from 'react'
 const FilePicker = ({file, setFile, readFile}) => {
   return (
     <div className='filepicker-container'>
+      <div className='flex flex-1 flex-col'>
         <input 
-            id='file-upload'
-            type='file'
-            accept='image/*'
-            value={file}
-            onChange={(e) => setFile(e.target.value)}
+          id='file-upload'
+          type='file'
+          accept='image/*'
+          value={file}
+          onChange={(e) => setFile(e.target.value)}
         />
         <label htmlFor='file-upload' className='filepicker-label'>
-            Upload File
+          Upload File
         </label>
-        <button 
-            className='filepicker-btn'
-            type='button'
-            onClick={() => readFile('file')}
-        >
-            Upload
-        </button>
-      
+        <p className='mt-2 text-gray-500 text-sm truncate'>
+          {file === '' ? 'No file selected' : file}
+        </p>
+      </div>
     </div>
   )
 }
