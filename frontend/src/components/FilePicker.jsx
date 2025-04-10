eed1c9f26c704abb52a648925f9214add280c1dc
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-04-10 23:55:03 +0530

    Custom UI for FilePicker added

--- frontend/src/components/FilePicker.jsx
+++ frontend/src/components/FilePicker.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const FilePicker = () => {
+const FilePicker = ({file, setFile, readFile}) => {
   return (
-    <div>
+    <div className='filepicker-container'>
+        <input 
+            id='file-upload'
+            type='file'
+            accept='image/*'
+            value={file}
+            onChange={(e) => setFile(e.target.value)}
+        />
+        <label htmlFor='file-upload' className='filepicker-label'>
+            Upload File
+        </label>
+        <button 
+            className='filepicker-btn'
+            type='button'
+            onClick={() => readFile('file')}
+        >
+            Upload
+        </button>
       
     </div>
   )
