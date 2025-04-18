8e67a95d4616c40ddede5e4455e5a8a8ab787713
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-04-18 23:51:48 +0530

    Code added

--- server/index.js
+++ server/index.js
@@ -15,3 +15,20 @@ app.get('/', (req, res) => {
 app.listen(8080, () => {
   console.log('Server is running on port http://localhost:8080');
 });
+
+app.post('/api/v1/dalle', async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+    const image = response.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
+});
